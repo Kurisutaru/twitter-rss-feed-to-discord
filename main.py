3fea0eabdce6e1fe0f44711b2a6ec06397efff9e
Varenchinusu Michael <michael.hansetyanto@kurisutaru.net>
2023-07-26 10:19:38 +0700

    Merge Conflict with new stuff

--- main.py
+++ main.py
@@ -29,36 +29,79 @@ __purikoneDiscordWebhookURL = "https://discord.com/api/webhooks/4820386913431060
 __blueProtocolUrlRss = "https://nitter.projectsegfau.lt/BLUEPROTOCOL_JP/rss"
 __blueprotocolDiscordWebhookURL = "https://discord.com/api/webhooks/1126817494833836112/Mxz4QOwVEvjhhFEhDEmu8zuwzuBpJx94Z-8QSn9VFOKblrL0cOPqWBPFJAIVvZyGMqyp"
 
-purikoneReDiveFeedData = feedparser.parse(__purikoneReDiveUrlRss).entries
-purikoneGWFeedData = feedparser.parse(__purikoneGWUrlRss).entries
+__purikoneGWKey = "PCRDGW"
+def replaceUrlToTwitter(inputString: str):
+    returnString = inputString.replace(__nitterUrl, __twitterUrl).replace('#m', '')
+
+    return returnString
+def generateTimestamp(inputTime):
+
+    return cal.timegm(inputTime)
+
+
+    returnString = inputString.replace(' / ', ' (') + str(")")
+def generateTwitterEmbedName(inputString: str):
+    return returnString
+
+
+def generateTwitterProfilePicture(inputString: str):
+    return returnString
+    returnString = inputString.replace('nitter.projectsegfau.lt/pic/', '').replace('%2F', '/')
+
+def generateEmbedData(title: str, authorName: str, authorUrl: str, authorIconUrl: str):
+
+    # create embed object for webhook
+
+    embed = DiscordEmbed(title=title)
+                     url=authorUrl,
+                     icon_url=authorIconUrl)
+    embed.set_author(name=authorName,
+
+    # set thumbnail
+
+    # set footer
+    embed.set_footer(text="Twitter", icon_url=__twitterImageURl)
+    #embed.set_thumbnail(url="your thumbnail url")
+
+    # set timestamp (default is now) accepted types are int, float and datetime
+
+    embed.set_timestamp()
+    return embed
+purikoneReDiveFeed = feedparser.parse(__purikoneReDiveUrlRss)
+
+
+blueprotocolFeed = feedparser.parse(__blueProtocolUrlRss)
+purikoneGWFeed = feedparser.parse(__purikoneGWUrlRss)
+purikoneReDiveFeedData = purikoneReDiveFeed.entries
+purikoneGWFeedData = purikoneGWFeed.entries
+
 blueprotocolFeedData = feedparser.parse(__blueProtocolUrlRss).entries
+
+
+# <link>https://nitter.projectsegfau.lt/pricone_GW</link>
+# <url>https://nitter.projectsegfau.lt/pic/pbs.twimg.com%2Fprofile_images%2F966599149217923072%2FgRr_MrRO_400x400.jpg</url>
+# <title>プリコネR攻略@GameWith / @pricone_GW</title>
+
+blueprotocolTwitterIconUrl = generateTwitterProfilePicture(blueprotocolFeed.feed.image.href)
+blueprotocolTwitterName = generateTwitterEmbedName(blueprotocolFeed.feed.image.title)
+blueprotocolTwitterLink = replaceUrlToTwitter(blueprotocolFeed.feed.image.link)
 
 con = sqlite3.connect(__sqliteDB)
 cur = con.cursor()
 
 entryData = []
 
-
-def replaceUrlToTwitter(inputString: str):
-    returnString = inputString.replace(__nitterUrl, __twitterUrl)
-    return returnString.replace('#m', '')
-
-
-def generateTimestamp(inputTime):
-    return cal.timegm(inputTime)
-
-
-for data in purikoneReDiveFeedData:
-    entryData.append((replaceUrlToTwitter(data.title), replaceUrlToTwitter(data.link), generateTimestamp(data.published_parsed), __purikoneKey))
-
-for data in purikoneGWFeedData:
-    entryData.append((replaceUrlToTwitter(data.title), replaceUrlToTwitter(data.link), generateTimestamp(data.published_parsed), __purikoneKey))
+con.commit()
+cur.executemany("INSERT OR IGNORE INTO post (title, link, pub_date, key) VALUES(?, ?, ?, ?)", entryData)
 
 for data in blueprotocolFeedData:
-    entryData.append((replaceUrlToTwitter(data.title), replaceUrlToTwitter(data.link), generateTimestamp(data.published_parsed), __blueprotocolKey))
 
-cur.executemany("INSERT OR IGNORE INTO post (title, link, pub_date, key) VALUES(?, ?, ?, ?)", entryData)
-con.commit()
+    entryData.append((replaceUrlToTwitter(data.title), replaceUrlToTwitter(data.link), generateTimestamp(data.published_parsed), __purikoneGWKey))
+for data in purikoneGWFeedData:
+
+    entryData.append((replaceUrlToTwitter(data.title), replaceUrlToTwitter(data.link), generateTimestamp(data.published_parsed), __purikoneKey))
+for data in purikoneReDiveFeedData:
+    entryData.append((replaceUrlToTwitter(data.title), replaceUrlToTwitter(data.link), generateTimestamp(data.published_parsed), __blueprotocolKey))
 
 # Reset Entry Data for reuseable
 entryData = []
@@ -77,5 +120,3 @@ for data in entryData:
 
 cur.executemany("UPDATE post SET is_send = 1 WHERE link = ?", [item.link for item in entryData])
 con.commit()
-
-
